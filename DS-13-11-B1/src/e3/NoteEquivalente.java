4fa1c55c2fb295fad879945ebefac946f8ac5b7c
javierpastoriza <92063431+javierpastoriza@users.noreply.github.com>
2021-10-08 09:47:02 +0200

    Add files via upload

---
+++ DS-13-11-B1/src/e3/NoteEquivalente.java
@@ -0,0 +1,11 @@
+package e3;
+
+protected package class NoteEquivalente
+{
+    Note notaA;
+    Accidentals alteracionA;
+    Note notaB;
+    Accidentals alteracionB;
+
+    //List<NoteEquivalente>
+}
